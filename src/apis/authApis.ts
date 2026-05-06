534aa03e53919cd15007111bb9102d16dbc848de
Shrey-Arpan <shreyarpan422.sa@gmail.com>
2026-05-06 14:49:55 +0530

    auth from backend

---
+++ src/apis/authApis.ts
@@ -0,0 +1,3 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
+
+export const ENTRA_AUTH_API = `${BASE_URL}/api/entra-auth`;
--- src/apis/dashboardApis.ts
+++ src/apis/dashboardApis.ts
@@ -1,4 +1,4 @@
-const AZURE_CODE = import.meta.env.VITE_AZURE_CODE;
+const AZURE_CODE = import.meta.env.VITE_API_AZURE_CODE;
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const DASHBOARD_LIST_API = `${BASE_URL}/api/files-list?code=${AZURE_CODE}`;
